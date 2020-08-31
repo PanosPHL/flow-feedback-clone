033aca869f8275c52f5ee8674a3d78445ec11ab9
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-08-31 17:41:08 -0400

    Refactored Login and Signup components as function components

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,43 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { login } from '../store/auth';
+import { useSelector, useDispatch } from 'react-redux';
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 
-class LogIn extends React.Component {
-    state = {
-        email: "",
-        password: ""
-    };
+const LogIn = () => {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const currentUserId = useSelector(state => state.auth.id);
+    const dispatch = useDispatch();
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.id.split('-')[1]]: event.target.value
-        });
-    }
-
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const { email, password } = this.state;
-        this.props.store.dispatch(login(email, password));
+        dispatch(login(email, password));
     }
 
-    render() {
-        return (
-            <MDBContainer>
-                <MDBRow>
-                    <MDBCol md='4'>
-                        <form onSubmit={this.props.handleSubmit} className='login-form'>
-                            <p className='h5 text-center mb-4'>Log In</p>
-                            <div className='grey-text'>
-                                <MDBInput onChange={this.handleChange} name='login-email' id='login-email' label='Type your email' icon='envelope' group type='email' value={this.state.email} />
-                                <MDBInput onChange={this.handleChange} name='login-password' id='login-password' label='Type your password' icon='lock' group type='password' value={this.state.password} />
-                            </div>
-                            <MDBBtn type='submit' className='btn amber darken-4'>Submit</MDBBtn>
-                        </form>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-        );
-    }
+    return (
+        <MDBContainer>
+            <MDBRow>
+                <MDBCol md='4'>
+                    <form onSubmit={handleSubmit} className='login-form'>
+                        <p className='h5 text-center mb-4'>Log In</p>
+                        <div className='grey-text'>
+                            <MDBInput onChange={(event) => setEmail(event.target.value)} name='login-email' id='login-email' label='Type your email' icon='envelope' group type='email' value={email} />
+                            <MDBInput onChange={(event) => setPassword(event.target.value)} name='login-password' id='login-password' label='Type your password' icon='lock' group type='password' value={password} />
+                        </div>
+                        <MDBBtn type='submit' className='btn amber darken-4'>Submit</MDBBtn>
+                    </form>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+    );
 }
 
 export default LogIn;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { signUp } from '../store/auth';
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 
-class SignUp extends React.Component {
-    state = {
-        email: "",
-        password: "",
-        confirmPassword: ""
+const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(signUp(email, password, confirmPassword))
     };
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.id.split('-')[1]]: event.target.value
-        });
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const { email, password, confirmPassword } = this.state;
-        this.props.store.dispatch(signUp(email, password, confirmPassword));
-    }
-
-    render() {
-        return (
+    return (
         <MDBContainer>
             <MDBRow>
                 <MDBCol md='4'>
-                    <form onSubmit={this.handleSubmit} className='signup-form'>
+                    <form onSubmit={handleSubmit} className='signup-form'>
                         <p className='h5 text-center mb-4'>Sign Up</p>
                         <div className='grey-text'>
-                            <MDBInput onChange={this.handleChange} name='signup-email' id='signup-email' label='Type your email' icon='envelope' group type='email' value={this.state.email} />
-                            <MDBInput onChange={this.handleChange} name='signup-password' id='signup-password' label='Type your password' icon='lock' group type='password' value={this.state.password} />
-                            <MDBInput onChange={this.handleChange} name='signup-confirmPassword' id='signup-confirmPassword' label='Type your password again' icon='check-double' group type='password' value={this.state.confirmPassword} />
+                            <MDBInput onChange={(event) => setEmail(event.target.value)} name='signup-email' id='signup-email' label='Type your email' icon='envelope' group type='email' value={email} />
+                            <MDBInput onChange={(event) => setPassword(event.target.value)} name='signup-password' id='signup-password' label='Type your password' icon='lock' group type='password' value={password} />
+                            <MDBInput onChange={(event) => setConfirmPassword(event.target.value)} name='signup-confirmPassword' id='signup-confirmPassword' label='Type your password again' icon='check-double' group type='password' value={confirmPassword} />
                         </div>
                         <MDBBtn type='submit' className='btn amber darken-4'>Submit</MDBBtn>
                     </form>
@@ -39,7 +31,6 @@ class SignUp extends React.Component {
             </MDBRow>
         </MDBContainer>
         );
-    }
-}
+};
 
 export default SignUp;
