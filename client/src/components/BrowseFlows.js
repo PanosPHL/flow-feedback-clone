c61ea45277aa611415d83b2ac543246dbe73526f
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-09-06 12:21:38 -0400

    Started work on browse flows page

---
+++ client/src/components/BrowseFlows.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+const BrowseMyFlows = () => {
+    const userId = useSelector(state => state.auth.id);
+    const [flows, setFlows] = useState({ flows: [] });
+
+    useEffect(() => {
+        const fetchFlows = async () => {
+            const res = await fetch(`/api/users/${userId}/flows`);
+
+            res.data = res.json();
+
+            if (res.ok) {
+                setFlows({ flows: res.data.Flows})
+            }
+        }
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BrowseFlows;
