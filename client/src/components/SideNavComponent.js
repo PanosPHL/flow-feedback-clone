fc44530e29dee7b29e3c5ea27da9b23fb5071241
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-09-06 17:29:09 -0400

    Added transition effect for sidebar

--- client/src/components/SideNavComponent.js
+++ client/src/components/SideNavComponent.js
@@ -1,20 +1,35 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Sidebar from 'react-sidebar';
+import styles from '../css-modules/SideNavComponent.module.css';
+import { MDBIcon } from 'mdbreact';
 
 const SideNavComponent = (props) => {
     const [open, setOpen] = useState(false);
+    const [className, setClassName] = useState('sidebarClosed');
 
-    const openSidebar = (open) => {
-        setOpen(true)
+    const openSidebar = () => {
+        setOpen(!open)
+    }
+
+    const toggleSidebar = () => {
+        if (className === 'sidebarClosed') {
+            setClassName('sidebarOpen');
+            return;
+        }
+        setClassName('sidebarClosed');
     }
 
     return (
         <Sidebar
-        sidebar={<b>Sidebar content</b>}
+        sidebar={
+        <button onClick={toggleSidebar} type='button' className='btn btn-sm btn-black'><MDBIcon icon='angle-right' /></button>
+        }
+        sidebarClassName={styles[className]}
+        docked={true}
             open={open}
             onSetOpen={openSidebar}
-            styles={{ sidebar: { background: 'grey'} }}
+            styles={{ sidebar: { background: 'grey', transition: "", WebkitTransition: ""} }}
         >
             <button type='button' className='btn' onClick={openSidebar}>Open sidebar</button>
         </Sidebar>
