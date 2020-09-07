c75947cd37765db0d1a8d60f649727dc61b6a2a0
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-09-07 17:53:26 -0400

    Styled not found page

--- client/src/components/CatCard.js
+++ client/src/components/CatCard.js
@@ -3,12 +3,13 @@ import { MDBCard, MDBCardImage } from 'mdbreact';
 import styles from '../css-modules/BrowseCats.module.css';
 import { Link } from 'react-router-dom';
 
-const CatCard = ({ cover, catId }) => {
+const CatCard = ({ cover, catId, name }) => {
     return (
         <Link to={`category/${catId}`}>
         <MDBCard className={styles.card + ' rounded'}>
             <MDBCardImage className='img-fluid' src={cover} />
         </MDBCard>
+        <span style={{color: 'black', marginTop: '8px', display: 'block'}}>{name}</span>
         </Link>
     )
 }
