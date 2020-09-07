cb154a4e95fff5ede96b13327cd42da8b326e6e5
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-09-07 18:53:11 -0400

    Added Overwatch videos

--- db/seeders/20200907220232-add-videos.js
+++ db/seeders/20200907220232-add-videos.js
@@ -12,7 +12,12 @@ module.exports = {
       { id: 'roTzaAhQTQM', sideId: 1, url: 'https://www.youtube.com/watch?v=roTzaAhQTQM', title: "Cloud 9 vs 100 Thieves | Week 1 Day 3 S10 LCS Summer 2020 | C9 vs 100 W1D3", duration: 1759, thumbnail: 'https://i.ytimg.com/vi/roTzaAhQTQM/hqdefault.jpg'},
       { id: 'H7xkzrTya9o', sideId: 1, url: 'https://www.youtube.com/watch?v=H7xkzrTya9o', title: "Fnatic vs G2 Esports (BEST GAME EVER!) - LEC Spring 2019 Week 9 Day 1 - FNC vs G2", duration: 3376, thumbnail: 'https://i.ytimg.com/vi/H7xkzrTya9o/hqdefault.jpg'},
       { id: 'k2OAqgaZxrU', sideId: 1, url: 'https://www.youtube.com/watch?v=k2OAqgaZxrU', title: "CLG vs Golden Guardians | Week 2 Day 1 S10 LCS Summer 2020 | CLG vs GG W2D1", duration: 2221, thumbnail: 'https://i.ytimg.com/vi/k2OAqgaZxrU/hqdefault.jpg'},
-      { id: 'yhI5j-rOy1Y', sideId: 1, url: 'https://www.youtube.com/watch?v=yhI5j-rOy1Y', title: "Immortals vs Evil Geniuses | Week 4 Day 3 S10 LCS Summer 2020 | IMT vs EG W4D3", duration: 3203, thumbnail: 'https://i.ytimg.com/vi/yhI5j-rOy1Y/hqdefault.jpg'}
+      { id: 'yhI5j-rOy1Y', sideId: 1, url: 'https://www.youtube.com/watch?v=yhI5j-rOy1Y', title: "Immortals vs Evil Geniuses | Week 4 Day 3 S10 LCS Summer 2020 | IMT vs EG W4D3", duration: 3203, thumbnail: 'https://i.ytimg.com/vi/yhI5j-rOy1Y/hqdefault.jpg'},
+      { id: '8ykWVR4IsaY', sideId: 1, url: 'https://www.youtube.com/watch?v=8ykWVR4IsaY', title: "Playoffs Round 3A | @LA Gladiators vs @Philadelphia Fusion | Playoffs Week 1 | NA Day 3", duration: 5382, thumbnail: 'https://i.ytimg.com/vi/8ykWVR4IsaY/hqdefault.jpg'},
+      { id: 'YPj_4m2Do0k', sideId: 1, url: 'https://www.youtube.com/watch?v=YPj_4m2Do0k', title: "FINALS | Paris Eternal vs Philadelphia Fusion | Summer Showdown | NA Day 3", duration: 12641, thumbnail: 'https://i.ytimg.com/vi/YPj_4m2Do0k/hqdefault.jpg'},
+      { id: 'AZr27iqWzWw', sideId: 1, url: 'https://www.youtube.com/watch?v=AZr27iqWzWw', title: "@London Spitfire vs @NYXL | Week 29 | APAC Day 1", duration: 4857, thumbnail: 'https://i.ytimg.com/vi/AZr27iqWzWw/hqdefault.jpg'},
+      { id: 'IJwz95WRTM8', sideId: 1, url: 'https://www.youtube.com/watch?v=IJwz95WRTM8', title: "San Francisco Shock vs Vancouver Titans | Week 25 | NA Day 2", duration: 4039, thumbnail: 'https://i.ytimg.com/vi/IJwz95WRTM8/hqdefault.jpg'},
+      { id: 'dBKmxcOo9KM', sideId: 1, url: 'https://www.youtube.com/watch?v=dBKmxcOo9KM', title: "Playoffs Round 1A | @Boston Uprising vs Houston@Outlaws | Playoffs Week 1 | NA Day 1", duration: 8012, thumbnail: 'https://i.ytimg.com/vi/dBKmxcOo9KM/hqdefault.jpg'}
     ])
   },
 
