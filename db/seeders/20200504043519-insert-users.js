1d406819e16b7519d3e03c9a6eaf1e01762247cd
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-09-07 19:09:37 -0400

    Added additional users

--- db/seeders/20200504043519-insert-users.js
+++ db/seeders/20200504043519-insert-users.js
@@ -22,7 +22,10 @@ module.exports = {
       r({ email: 'demo@example.com', hashedPassword: createPassword() }),
       r({ email: 'yusuke@example.com', hashedPassword: createPassword() }),
       r({ email: 'petra@example.com', hashedPassword: createPassword() }),
-      r({ email: 'panosssbm@gmail.com', hashedPassword: createPassword('panosPass') })
+      r({ email: 'panosssbm@gmail.com', hashedPassword: createPassword('panosPass') }),
+      r({ email: 'faker@t1.com', hashedPassword: createPassword('fakerDaBess')}),
+      r({ email: 'carpe@phillyfusion.com', hashedPassword: createPassword('brotherlyLove')}),
+      r({ email: 'stewie2k@teamliquid.com', hashedPassword: createPassword('fragger')})
     ]);
   },
 
