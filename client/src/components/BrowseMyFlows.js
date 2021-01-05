7301d02d89e0a19ce7e38e601ca88274dad44945
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-05 13:42:15 -0500

    Styled browse pages for laptop screens

--- client/src/components/BrowseMyFlows.js
+++ client/src/components/BrowseMyFlows.js
@@ -4,29 +4,32 @@ import styles from '../css-modules/BrowseFlows.module.css';
 import BrowseFlowCard from './BrowseFlowCard';
 
 const BrowseMyFlows = () => {
-    const userId = useSelector(state => state.session.id);
-    const flows = useSelector(state => Object.values(state.entities.flows).filter((flow) => flow.userId === userId));
+  const userId = useSelector((state) => state.session.id);
+  const flows = useSelector((state) =>
+    Object.values(state.entities.flows).filter((flow) => flow.userId === userId)
+  );
 
-    return (
-        <div className={styles.pageContainer}>
-            <div className={styles.headerContainer}>
-            <h2 className={styles.catHeader}>My Flows</h2>
-            </div>
-        <div className={styles.cardContainer}>
-            {flows.length ?
-                flows.map((flow, i) => {
-                    return (
-                            <BrowseFlowCard
-                            key={`card-${i + 1}`}
-                            i={i}
-                            flow={flow}
-                            myFlow={true}/>
-                    )
-                })
-                : <> </>}
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.pageContainer}>
+      <h2 className={styles.catHeader}>My Flows</h2>
+      <div className={styles.cardContainer}>
+        {flows.length ? (
+          flows.map((flow, i) => {
+            return (
+              <BrowseFlowCard
+                key={`card-${i + 1}`}
+                i={i}
+                flow={flow}
+                myFlow={true}
+              />
+            );
+          })
+        ) : (
+          <> </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default BrowseMyFlows;
